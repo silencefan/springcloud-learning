1ee08d937b953d3126d8d6dae766b42da1471e8b
felix.fan <felix.fan@corp.to8to.com>
2019-02-14 14:19:20 +0800

    initial commit

---
+++ springcloud-learning-eureka-client/src/main/java/com/felixfan/learn/EurekaClientApplication.java
@@ -0,0 +1,32 @@
+package com.felixfan.learn;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: 服务提供者
+ * @author: felix.fan
+ * @date: 2019/2/14 11:20
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+}
