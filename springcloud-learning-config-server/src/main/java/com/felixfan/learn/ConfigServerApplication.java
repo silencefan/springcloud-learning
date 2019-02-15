a61ebf412737520bc797b0e2c3cb7bb8b15663e1
felix.fan <felix.fan@corp.to8to.com>
2019-02-15 15:24:57 +0800

    增加config模块

---
+++ springcloud-learning-config-server/src/main/java/com/felixfan/learn/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.felixfan.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author: felix.fan
+ * @date: 2019/2/15 09:22
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
